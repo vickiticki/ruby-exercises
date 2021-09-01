e46be4779e1686d1a993df73705f21ccefe36aed
Victoria Tiller <vijotiller@gmail.com>
2021-09-01 18:32:23 -0400

    nested arrays

--- ruby_basics/11_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/11_nested_collections/exercises/nested_array_exercises.rb
@@ -11,16 +11,21 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 
   # NOTE: if one of the nested arrays is changed, the others should **not**
   # change with it
+  Array.new(number_of_rows){Array.new(seats_per_row)}
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+  chart[row_index].push(seat_to_add)
+  chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+  chart.push(row_to_add)
+  chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
@@ -28,17 +33,33 @@ def delete_seat_from_row(chart, row_index, seat_index)
   # the chart, then return the chart
 
   # Hint: explore the ruby docs to find a method for deleting from an array!
+  chart[row_index].delete_at(seat_index)
+  chart
+
 end
 
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+  chart.delete_at(row_index)
+  chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
 
   # NOTE: `chart` should **not** be mutated
+  num = 0
+
+ chart.each_with_index do |row, row_index|
+  row.each_with_index do |chair, col_index|
+    if chair == nil
+      num += 1
+    end
+  end
+end
+return num
+
 end
 
 def find_favorite(array_of_hash_objects)
@@ -55,4 +76,23 @@ def find_favorite(array_of_hash_objects)
 
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
+  
+  # array_of_hash_objects.each do |hash = nil|
+  #   hash.select { |key, value| value == true}
+  #   return hash
+  # end
+ 
+  # array_of_hash_objects.each do |hash|
+  #   hash.select do |key, value|
+  #     if value == true
+  #       return hash
+  #     else
+  #       return nil
+  #     end
+  #   end
+  # end
+
+  array_of_hash_objects.find{|hash| hash[:is_my_favorite?] == true}
+  
+
 end
